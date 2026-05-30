35492ee24c445977db95b40d62cbf82e26ddcd03
LuubPEPE <lukejordan945@gmail.com>
2026-05-30 18:21:08 +0200

    Created basic layout for the maintain posts file and created the login.php file with basic layout

---
+++ login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<body>
+    <h1>Login</h1>
+    <form>
+        <label>Username</label>
+        <input placeholder="Enter Username">
+        <label>Password</label>
+        <input placeholder="Enter Password">
+    </form>
+</body>
